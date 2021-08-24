2a1a6b19da60bf24ab8974bdd9d447cf134be954
Justin Zheng <83163126+justinzzy-code@users.noreply.github.com>
2021-08-24 23:28:48 +0800

    Update ai_creation.py

--- ai_creation.py
+++ ai_creation.py
@@ -9,7 +9,7 @@ import numpy as np
 from collections import deque
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam
 import os
 
 
